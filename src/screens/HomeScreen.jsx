0f86f7d7b9b2b9d9682d7bd70a40e059d0ee29fb
omerglck <omergolcek10@gmail.com>
2024-06-08 20:14:12 +0300

    first commit

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+import Header from '../components/Header';
+import SubHeader from '../components/SubHeader';
+import Category from '../components/Category';
+import Carousel from '../components/Carousel';
+
+export default function HomeScreen() {
+  return (
+    <ScrollView>
+      <Header />
+      <SubHeader />
+      <Category />
+      <Carousel />
+    </ScrollView>
+  );
+}
