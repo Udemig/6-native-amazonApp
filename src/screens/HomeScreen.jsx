b37831e66299997679bb99675c42a64ef8d547aa
omerglck <omergolcek10@gmail.com>
2024-06-09 18:09:45 +0300

    finish

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,14 +4,22 @@ import Header from '../components/Header';
 import SubHeader from '../components/SubHeader';
 import Category from '../components/Category';
 import Carousel from '../components/Carousel';
+import Services from '../components/Services';
+import Deals from '../components/Deals';
+import Brands from '../components/Brands';
 
 export default function HomeScreen() {
   return (
-    <ScrollView>
-      <Header />
-      <SubHeader />
-      <Category />
-      <Carousel />
-    </ScrollView>
+    <SafeAreaView>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header />
+        <SubHeader />
+        <Category />
+        <Carousel />
+        <Services />
+        <Deals />
+        <Brands />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
